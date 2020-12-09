35da6d7d21f8d05a55270d491ee1ace0c628d3d3
Joseph Vober <jdvober@gmail.com>
2020-12-09 17:09:13 -0500

    Up

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
@@ -42,7 +43,7 @@ func init() {
 func main() {
 	// Load data from env file
 	giftedSSID := os.Getenv("GIFTED_SSID")
-	wepTemplateSSID := os.Getenv("WEP_TEMPLATE_SSID")
+	// wepTemplateSSID := os.Getenv("WEP_TEMPLATE_SSID")
 	gr := os.Getenv("GRADE")
 	grad := os.Getenv("GRADUATION_YEAR")
 	st := os.Getenv("START")
@@ -55,12 +56,14 @@ func main() {
 
 	// Iterate through data and format for paste in WEP Template Google Sheet
 	values := make([][]interface{}, len(data))
+	profiles := make([]Student, len(data))
 	for i := range data {
 		// Create student profile
 		profile := Student{
 			firstName: data[i][1].(string),
 			lastName:  data[i][0].(string),
 			superCog:  data[i][2].(string),
+			creative:  data[i][3].(string),
 			science:   data[i][12].(string),
 			name:      strings.Join([]string{data[i][1].(string), data[i][0].(string)}, " "),
 			grade:     gr,
@@ -91,6 +94,88 @@ func main() {
 		}
 		// Add to array to post to sheet
 		values[i] = []interface{}{profile.AP, profile.name, profile.grade, profile.target, profile.start, profile.end, profile.review, profile.academic}
+
+		profiles = append(profiles, profile)
+		// Make local WEP XML file
 	}
-	gsheets.BatchUpdateValues(client, wepTemplateSSID, "Sheet1!A2:H", "ROWS", values)
+	makeFiles(profiles)
+	// Post to sheet
+	// gsheets.BatchUpdateValues(client, wepTemplateSSID, "Sheet1!A2:H", "ROWS", values)
+}
+
+func makeFiles(profiles []Student) {
+
+	for _, profile := range profiles {
+		var filename string = profile.firstName + "_" + profile.lastName + "_" + "WEP.xml"
+
+		if _, err := os.Stat("./weps"); os.IsNotExist(err) {
+			fmt.Println("./weps does not exist. Mkdir will create ./weps")
+			os.Mkdir("./weps/", 0777)
+		}
+		// Open the proper template as a byte array
+		if profile.AP == "true" {
+			src, err := ioutil.ReadFile("./templates/AP.xml")
+			if err != nil {
+				log.Println("Error reading file ./templates/AP.XML")
+				return
+			}
+			// Find and replace
+			newFile := strings.ReplaceAll(string(src), "$NAME$", profile.name)
+			newFile = strings.ReplaceAll(newFile, "$GRADE$", profile.grade)
+			newFile = strings.ReplaceAll(newFile, "$TARGET$", profile.target)
+			newFile = strings.ReplaceAll(newFile, "$START$", profile.start)
+			newFile = strings.ReplaceAll(newFile, "$END$", profile.end)
+			newFile = strings.ReplaceAll(newFile, "$REVIEW$", profile.review)
+			newFile = strings.ReplaceAll(newFile, "$ACADEMIC$", profile.academic)
+
+			f, err := os.Create("./weps/" + filename)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Printf("\nCreated file ./weps/%s\n", filename)
+			l, err := f.WriteString(newFile)
+			if err != nil {
+				fmt.Printf("Problem writing %v bytes", l)
+				log.Println(err)
+				f.Close()
+				return
+			}
+			err = f.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if profile.honors == "true" {
+			src, err := ioutil.ReadFile("./templates/Honors.xml")
+			if err != nil {
+				log.Println("Error reading file ./templates/Honors.XML")
+				return
+			}
+			// Find and replace
+			newFile := strings.ReplaceAll(string(src), "$NAME$", profile.name)
+			newFile = strings.ReplaceAll(newFile, "$GRADE$", profile.grade)
+			newFile = strings.ReplaceAll(newFile, "$TARGET$", profile.target)
+			newFile = strings.ReplaceAll(newFile, "$START$", profile.start)
+			newFile = strings.ReplaceAll(newFile, "$END$", profile.end)
+			newFile = strings.ReplaceAll(newFile, "$REVIEW$", profile.review)
+			newFile = strings.ReplaceAll(newFile, "$ACADEMIC$", profile.academic)
+
+			f, err := os.Create("./weps/" + filename)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Printf("\nCreated file ./weps/%s\n", filename)
+			l, err := f.WriteString(newFile)
+			if err != nil {
+				fmt.Printf("Problem writing %v bytes", l)
+				log.Println(err)
+				f.Close()
+				return
+			}
+			err = f.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+	}
 }
